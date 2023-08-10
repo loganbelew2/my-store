0f35f5bc9be11ecfdee5e7b4518f85d990a4592c
Logan Belews <Loganbelew2@gmail.com>
2023-08-09 23:55:24 -0500

    added column views

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
   styleUrls: ['./products-header.component.scss']
 })
 export class ProductsHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter<number>();
   sort = 'desc';
   itemsShowCount = 12;
 
@@ -15,5 +15,9 @@ export class ProductsHeaderComponent {
 
   OnItemsUpdated(count: number): void {
     this.itemsShowCount = count
+  }
+
+  onColumnsUpdated(colsNum: number): void{
+    this.columnsCountChange.emit(colsNum) 
   }
 }
