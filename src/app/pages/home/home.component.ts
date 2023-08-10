90964481779dbacbedff2f2e70f105e1e390aceb
Logan Belews <Loganbelew2@gmail.com>
2023-08-09 23:54:47 -0500

    added event to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  cols = 3;
 
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum
+  }
 }
